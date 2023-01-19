7ae06d10b8dcf8a1a33bd71b5d00d2cbb6dafc19
Marko Matura <Marko.Matura@gmail.com>
2023-01-18 19:10:37 -0500

    Add solution to 09_palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    newStr = str.replace(/[^A-Za-z0-9]/g, "").replace(/\s{2,}/g, "").toLowerCase();
+    reverseStr = newStr.split('').reverse().join('');
 
+    if (newStr === reverseStr) {
+        return true
+    } else {
+        return false
+    }
 };
 
 // Do not edit below this line
