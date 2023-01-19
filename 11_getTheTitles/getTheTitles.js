b0847b16675baf28507472f3e37dfe7a41dfb4f0
Marko Matura <Marko.Matura@gmail.com>
2023-01-19 12:29:42 -0500

    Add solution to 11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,13 @@
-const getTheTitles = function() {
+const getTheTitles = function (array) {
+    
+    //return bookArray.push(books["title"]);
+        
+    //return books.map(title => title.book)
 
+    return array.map(book => book.title);
 };
+    
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
