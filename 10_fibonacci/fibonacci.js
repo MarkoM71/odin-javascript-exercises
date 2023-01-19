f2d1976d67f7f44d23ad6e5dc373034932b9b573
Marko Matura <Marko.Matura@gmail.com>
2023-01-19 11:03:33 -0500

    Add solution to 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+    let a = 0;
+    let b = 1;
+    let c;
 
+    if (Math.sign(num) === -1) {
+        return "OOPS";
+    }
+
+    else if (typeof num === "string") {
+        parseInt(num);
+    }
+
+    for (let i = 1; i <= num; i++){
+        c = a + b;
+        a = b;
+        b = c;                          /// a, b, c
+    }
+    return a;
 };
 
 // Do not edit below this line
